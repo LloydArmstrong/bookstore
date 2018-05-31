90b5d7452c13c4eeec37f6c1d2a0842e537bdbdb
Lloyd Armstrong <lloydarmstrong@gmail.com>
2018-05-31 14:24:17 +0200

    Added Books and Genre Collections on Mongo

--- app.js
+++ app.js
@@ -3,13 +3,29 @@ var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
+Genre = require("./models/genre.js");
+
 // Connect to Mongoose
 mongoose.connect('mongodb://localhost/bookstore');
-var db = mongoose.connection
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    // we're connected!
+});
 
 app.get('/', function (req, res) {
     res.send('Please use /api/books or /api/genres');
 });
 
+app.get('/api/genres', function (req, res) {
+    Genre.getGenres(function (err, genres) {
+        if(err){
+            throw err;
+        }
+        res.json(genres);
+    })
+});
+
 app.listen(3000);
 console.log('Running server on port 3000...');
+
