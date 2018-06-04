42462fff5a7c0f2db5479f676263687faf92c9e3
Lloyd Armstrong <lloydarmstrong@gmail.com>
2018-06-04 14:59:01 +0200

    Completed RESTful API for Bookstore

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
-Genre = require("./models/genre.js");
+app.use(bodyParser.json());
+
+Genre = require("./models/genre");
+Book = require("./models/book");
 
 // Connect to Mongoose
 mongoose.connect('mongodb://localhost/bookstore');
@@ -23,6 +26,86 @@ app.get('/api/genres', function (req, res) {
             throw err;
         }
         res.json(genres);
+    })
+});
+
+app.post('/api/genres', function (req, res) {
+    var genre = req.body;
+    Genre.addGenre(genre, function (err, genre) {
+        if(err){
+            throw err;
+        }
+        res.json(genre);
+    })
+});
+
+app.put('/api/genres/:_id', function (req, res) {
+    var id = req.params._id;
+    var genre = req.body;
+    Genre.updateGenre(id, genre, {}, function (err, genre) {
+        if(err){
+            throw err;
+        }
+        res.json(genre);
+    })
+});
+
+app.delete('/api/genres/:_id', function (req, res) {
+    var id = req.params._id;
+    Genre.removeGenre(id, {}, function (err, genres) {
+        if(err){
+            throw err;
+        }
+        res.json(genre);
+    })
+});
+
+app.get('/api/books', function (req, res) {
+    Book.getBooks(function (err, books) {
+        if(err){
+            throw err;
+        }
+        res.json(books);
+    })
+});
+
+app.get('/api/books/:_id', function (req, res) {
+    Book.getBookById(req.params._id, function (err, book) {
+        if(err){
+            throw err;
+        }
+        res.json(book);
+    })
+});
+
+app.post('/api/books', function (req, res) {
+    var book = req.body;
+    Book.addBook(book, function (err, book) {
+        if(err){
+            throw err;
+        }
+        res.json(book);
+    })
+});
+
+app.put('/api/books/:_id', function (req, res) {
+    var id = req.params._id;
+    var book = req.body;
+    Book.updateBook(id, book, {}, function (err, book) {
+        if(err){
+            throw err;
+        }
+        res.json(book);
+    })
+});
+
+app.delete('/api/books/:_id', function (req, res) {
+    var id = req.params._id;
+    Book.removeBook(id, {}, function (err, books) {
+        if(err){
+            throw err;
+        }
+        res.json(book);
     })
 });
 
--- models/genre.js
+++ models/genre.js
@@ -12,9 +12,29 @@ var genreSchema = mongoose.Schema({
     }
 });
 
-var Genre = module.exports = mongoose.model("Genre", genreSchema);
+var Genre = module.exports = mongoose.model('Genre', genreSchema);﻿
 
 // Get Genres
 module.exports.getGenres = function (callback, limit) {
     Genre.find(callback).limit(limit);
+};
+
+// Add Genre
+module.exports.addGenre = function (genre, callback) {
+    Genre.create(genre, callback);
+};
+
+// Update Genre
+module.exports.updateGenre = function (id, genre, options, callback) {
+    var query = {_id: id};
+    var update = {
+        name: genre.name
+    }
+    Genre.findOneAndUpdate(query, update, options, callback);
+};
+
+// Delete Genre
+module.exports.removeGenre = function (id, callback) {
+    var query = {_id: id};
+    Genre.remove(query, callback);
 };
